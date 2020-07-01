2b739ac347d3989a548fe757b0e56e22ed017e31
T4nkUwU <orafaeldiaz@gmail.com>
2020-06-30 23:25:41 -0400

    Added y bounds, descriptive error states

--- GameState.py
+++ GameState.py
@@ -1,24 +1,37 @@
 class meeple:
-    def __init__(self, location, board):
-        self.location = location
+    def __init__(self, xStart, yStart, board):
+        self.xLocation = xStart
+        self.yLocation = yStart
         self.health = 6
         self.board = board
 
     def myLocation(self):
-        print(str(self.location))
+        print("Your current board position is X:" + str(self.xLocation) + " And Y : " + str(self.yLocation))
 
     def actionFunction(self, action):
-        if action.lower() == 'move left' and self.location >= 2:
-            self.location -= 1
-            self.myLocation()
-        elif self.location == 1:
-            print("Illegal manuever!")
+        if action.lower() == 'move left' and self.xLocation >= 2:
+            self.xLocation -= 1
+            return self.myLocation()
+        elif action.lower() == 'move left' and self.xLocation == 1:
+            return print("Illegal manuever: Cannot move past left bounds")
 
-        if action.lower() == 'move right' and (self.board.xMax - 1) >= self.location:
-            self.location += 1
-            self.myLocation()
-        elif self.location == self.board.xMax:
-            print("Illegal manuever!")
+        if action.lower() == 'move right' and (self.board.xMax - 1) >= self.xLocation:
+            self.xLocation += 1
+            return self.myLocation()
+        elif action.lower() == 'move right' and self.xLocation == self.board.xMax:
+            return print("Illegal manuever: Cannot move past left bounds")
+
+        if action.lower() == 'move down' and self.yLocation >= 2:
+            self.yLocation -= 1
+            return self.myLocation()
+        elif action.lower() == 'move down' and self.yLocation == 1:
+            return print("Illegal manuever: Cannot move below bottom bounds")
+
+        if action.lower() == 'move up' and (self.board.yMax - 1) >= self.yLocation:
+            self.yLocation += 1
+            return self.myLocation()
+        elif action.lower() == 'move up' and self.yLocation == self.board.yMax:
+            return print("Illegal manuever: Cannot move above top bounds")
 
 
 class board:
@@ -29,7 +42,7 @@ class board:
 
 
 newBoard = board(7,1)
-redMeeple = meeple(3, newBoard)
+redMeeple = meeple(3,1, newBoard)
 gameLoop = True
 
 while(gameLoop == True):
