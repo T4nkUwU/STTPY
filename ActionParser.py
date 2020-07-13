1c010a015cc3223169f094d7373471eb28d8d692
T4nkUwU <orafaeldiaz@gmail.com>
2020-07-12 23:09:42 -0400

    Action parser progress

--- ActionParser.py
+++ ActionParser.py
@@ -5,15 +5,50 @@ class Action:
             self.abreviation = str(self.abreviation).lower()
         elif len(str(abv)) == 3:
             self.abreviation = str(abv).lower()
-        self.actionName = str(name).capitalize
-        self.description = str(description).capitalize
+        self.actionName = str(name).capitalize()
+        self.description = str(description).capitalize()
     
     def retChar(self):
         return(str(self.actionName)[0])
 
+    def helpMe(self):
+        return(print(f'{self.actionName} ({self.abreviation}): {self.description}'))
+
+    def runAction(self):
+        return(print(f'\n{self.actionName}: {self.description}\n'))
+
 class ActionParser:
-    def __init__(self):
-        self.Announce = Action("Announce", "anc", "Announce your presence to the world!")
+    def __init__(self, *Actions):
+        self.ActionsList = []
+        self.alphabet = ''
+        for action in Actions:
+            self.ActionsList.append(action)
+            self.alphabet += str(action.actionName)[0]
+        ##self.ActionTable = []
+        ##for i in self.ActionsList:
+        ##    if 
+        
+    def CommandList(self):
+        print("\n\n=== LIST OF USEABLE COMMANDS===")
+        for Action in self.ActionsList:
+            Action.helpMe()
+        print("=== END OF LIST ==\n")
+
+    def getInput(self, input):
+        if input == "help":
+            return self.CommandList()
+        if len(str(input)) == 3:
+            for Action in self.ActionsList:
+                if Action.abreviation == input:
+                    return Action.runAction()
+        
 
 soldier = Action("Soulja boy", "soj", "Up in dat ho")
-g = input("Input an action! \n")
+Announce = Action("Announce", "anc", "Announce your presence to the world!")
+
+todaysParser = ActionParser(soldier,Announce)
+
+game = True
+while game == True:
+    g = input("Input an action! \n")
+    todaysParser.getInput(g)
