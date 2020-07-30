1926696ef595d4b86b6ef9dbad626587cd5a8342
T4nkUwU <orafaeldiaz@gmail.com>
2020-07-30 10:59:20 -0400

    Versioning to help with production

--- GameState.py
+++ GameState.py
@@ -2,95 +2,104 @@ from random import *
 import math
 from os import system, name
 
-class meeple:
-    def __init__(self, name, xStart, yStart):
-        self.xLocation = xStart
-        self.yLocation = yStart
-        ##self.health = 6
-        self.name = name
-
-    def myLocation(self):
-        print(f'{self.name}s current position is X: {self.xLocation} And Y : {self.yLocation}')
-
-class board:
-    def __init__(self, xMax, yMax):
-        self.xMax = xMax
-        self.yMax = yMax
-
-class player:
-    def __init__(self, name):
-        self.name = name
-
-class inputReader:
-    def __init__(self, player, meeple, board, gameState):
-        self.numberOfPlayers = 1
-        self.thePlayer = player
-        self.theMeeple = meeple
-        self.theBoard = board
-        self.theGame = gameState
-        self.resetVictory()
-        self.theMeeple.myLocation()
-
-    def winCheck(self):
-        if self.winningX == self.theMeeple.xLocation and self.winningY == self.theMeeple.yLocation:
-            return self.theGame.winGame()
-        elif self.winningX != self.theMeeple.xLocation or self.winningY != self.theMeeple.yLocation:
-            return
-
-    def actions(self, input):
-        if input.lower() == 'move left' and self.theMeeple.xLocation >= 2:
-            self.theMeeple.xLocation -= 1
-            self.winCheck()
-            return self.theMeeple.myLocation()
-        elif input.lower() == 'move left' and self.theMeeple.xLocation == 1:
-            return print("Illegal manuever: Cannot move past left bounds")
-
-        if input.lower() == 'move right' and (self.theBoard.xMax - 1) >= self.theMeeple.xLocation:
-            self.theMeeple.xLocation += 1
-            self.winCheck()
-            return self.theMeeple.myLocation()
-        elif input.lower() == 'move right' and self.theMeeple.xLocation == self.theBoard.xMax:
-            return print("Illegal manuever: Cannot move past left bounds")
-
-        if input.lower() == 'move down' and self.theMeeple.yLocation >= 2:
-            self.theMeeple.yLocation -= 1
-            self.winCheck()
-            return self.theMeeple.myLocation()
-        elif input.lower() == 'move down' and self.theMeeple.yLocation == 1:
-            return print("Illegal manuever: Cannot move below bottom bounds")
-
-        if input.lower() == 'move up' and (self.theBoard.yMax - 1) >= self.theMeeple.yLocation:
-            self.theMeeple.yLocation += 1
-            self.winCheck()
-            return self.theMeeple.myLocation()
-        elif input.lower() == 'move up' and self.theMeeple.yLocation == self.theBoard.yMax:
-            return print("Illegal manuever: Cannot move above top bounds")
-
-    def resetVictory(self):
-        system('cls')
-        self.winningX = randrange(1,self.theBoard.xMax)
-        self.winningY = randrange(1,self.theBoard.yMax)
-        self.theMeeple.yLocation = int(math.ceil(self.theBoard.yMax/2))
-        self.theMeeple.xLocation = int(math.ceil(self.theBoard.yMax/2))
-        return
-
 class gameState:
-    def __init__(self):
-        self.gameLoop = True
-        self.curPlayer = player(input("Tell me your name! \n"))
-        self.curBoard = board(7,7)
-        self.curMeeple = meeple(str(self.curPlayer.name),int(math.ceil(self.curBoard.xMax/2)),int(math.ceil(self.curBoard.yMax/2)))
-        self.curReader = inputReader(self.curPlayer, self.curMeeple, self.curBoard, self)
-        self.runGame()
+     def __init__(self):
+        print("Welcome to game state! This is the prototype of the STT system.\n\n")
+        building = True
+        while building == True:
+            self.numberOfPlayers = input("How many players are playing?:\n")
+            if isinstance(self.numberOfPlayers, int)
+            
+
+# class meeple:
+#     def __init__(self, name, xStart, yStart):
+#         self.xLocation = xStart
+#         self.yLocation = yStart
+#         self.health = 6
+#         self.name = name
+
+#     def myLocation(self):
+#         print(f'{self.name}s current position is X: {self.xLocation} And Y : {self.yLocation}')
+
+# class board:
+#     def __init__(self, xMax, yMax):
+#         self.xMax = xMax
+#         self.yMax = yMax
+
+# class player:
+#     def __init__(self, name):
+#         self.name = name
+
+# class inputReader:
+#     def __init__(self, player, meeple, board, gameState):
+#         self.numberOfPlayers = 1
+#         self.thePlayer = player
+#         self.theMeeple = meeple
+#         self.theBoard = board
+#         self.theGame = gameState
+#         self.resetVictory()
+#         self.theMeeple.myLocation()
+
+#     def winCheck(self):
+#         if self.winningX == self.theMeeple.xLocation and self.winningY == self.theMeeple.yLocation:
+#             return self.theGame.winGame()
+#         elif self.winningX != self.theMeeple.xLocation or self.winningY != self.theMeeple.yLocation:
+#             return
+
+#     def actions(self, input):
+#         if input.lower() == 'move left' and self.theMeeple.xLocation >= 2:
+#             self.theMeeple.xLocation -= 1
+#             self.winCheck()
+#             return self.theMeeple.myLocation()
+#         elif input.lower() == 'move left' and self.theMeeple.xLocation == 1:
+#             return print("Illegal manuever: Cannot move past left bounds")
+
+#         if input.lower() == 'move right' and (self.theBoard.xMax - 1) >= self.theMeeple.xLocation:
+#             self.theMeeple.xLocation += 1
+#             self.winCheck()
+#             return self.theMeeple.myLocation()
+#         elif input.lower() == 'move right' and self.theMeeple.xLocation == self.theBoard.xMax:
+#             return print("Illegal manuever: Cannot move past left bounds")
+
+#         if input.lower() == 'move down' and self.theMeeple.yLocation >= 2:
+#             self.theMeeple.yLocation -= 1
+#             self.winCheck()
+#             return self.theMeeple.myLocation()
+#         elif input.lower() == 'move down' and self.theMeeple.yLocation == 1:
+#             return print("Illegal manuever: Cannot move below bottom bounds")
+
+#         if input.lower() == 'move up' and (self.theBoard.yMax - 1) >= self.theMeeple.yLocation:
+#             self.theMeeple.yLocation += 1
+#             self.winCheck()
+#             return self.theMeeple.myLocation()
+#         elif input.lower() == 'move up' and self.theMeeple.yLocation == self.theBoard.yMax:
+#             return print("Illegal manuever: Cannot move above top bounds")
+
+#     def resetVictory(self):
+#         system('cls')
+#         self.winningX = randrange(1,self.theBoard.xMax)
+#         self.winningY = randrange(1,self.theBoard.yMax)
+#         self.theMeeple.yLocation = int(math.ceil(self.theBoard.yMax/2))
+#         self.theMeeple.xLocation = int(math.ceil(self.theBoard.yMax/2))
+#         return
+
+# class gameState:
+#     def __init__(self):
+#         self.gameLoop = True
+#         self.curPlayer = player(input("Tell me your name! \n"))
+#         self.curBoard = board(7,7)
+#         self.curMeeple = meeple(str(self.curPlayer.name),int(math.ceil(self.curBoard.xMax/2)),int(math.ceil(self.curBoard.yMax/2)))
+#         self.curReader = inputReader(self.curPlayer, self.curMeeple, self.curBoard, self)
+#         self.runGame()
     
-    def runGame(self):
-        while(self.gameLoop == True):
-            g = input("Input an action! \n")
-            self.curReader.actions(str(g))
+#     def runGame(self):
+#         while(self.gameLoop == True):
+#             g = input("Input an action! \n")
+#             self.curReader.actions(str(g))
 
     
-    def winGame(self):
-        input("Congrats on winning! \nPress any key to start a new game\n\n")
-        self.curReader.resetVictory()
+#     def winGame(self):
+#         input("Congrats on winning! \nPress any key to start a new game\n\n")
+#         self.curReader.resetVictory()
     
 currentGame = gameState()
